ee3b9bcc9422a08ec6bc2b77706b2701641aad14
yunluoxin <hhdgw@qq.com>
2016-01-23 15:30:42 +0800

    Initial Commit

---
+++ DDAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDAlertView
+//
+//  Created by 张小冬 on 16/1/23.
+//  Copyright © 2016年 dadong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDAlertView
+//
+//  Created by 张小冬 on 16/1/23.
+//  Copyright © 2016年 dadong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
